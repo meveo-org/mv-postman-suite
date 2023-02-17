dc738036a10f5450c8f0ae4e6fb1a2513ba2401e
Nhan <mtnhan@gmail.com>
2023-02-17 17:02:42 +0700

    #625 Init project with some cet

---
+++ facets/java/org/meveo/model/customEntities/postmanEnvironment.java
@@ -0,0 +1,76 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.io.Serializable;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class postmanEnvironment implements CustomEntity, Serializable {
+
+    public postmanEnvironment() {
+    }
+
+    public postmanEnvironment(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    @JsonProperty(required = true)
+    private String code;
+
+    private String content;
+
+    private String contentHash;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getContentHash() {
+        return contentHash;
+    }
+
+    public void setContentHash(String contentHash) {
+        this.contentHash = contentHash;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "postmanEnvironment";
+    }
+}
